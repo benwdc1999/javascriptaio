ba1c1cc4bbb5a9a2b8efacb7f9a44a144c5e577f
benjamin <benbhbe@yahoo.com>
2023-09-28 12:31:00 -0400

    redid files

--- gamelet.js
+++ gamelet.js
@@ -1,31 +1,35 @@
-const ball = document.getElementById('ball');
-document.addEventListener('keydown', handleKeyPress);
+const ball = document.getElementById("ball");
+document.addEventListener("keydown", handleKeyPress);
 let position = 0;
-let yPosition = 0;
-function handleKeyPress(e) {
-    if (e.code === 'ArrowLeft'){
-        position = position - 20;
-    }
-    if (e.code === 'ArrowRight'){
-        position = position + 20;
-    }
-    if (position < 0){
-        position = 0;
-    }
-    if (e.code === 'ArrowUp'){
-        yPosition = yPosition - 20;
-    }
-    if (e.code === "ArrowDown"){
-        yPosition = yPosition + 20;
-    }
-    if (yPosition < 0 ){
-        yPosition = 0;
-    }
+let verticalPosition = 0;
 
-    refresh();
+function handleKeyPress(e) {
+  if (e.code === "KeyS") {
+    position = position - 15;
+  }
+  if (e.code === "KeyD") {
+    position = position + 15;
+  }
+
+  if (e.code === "KeyE") {
+    verticalPosition = verticalPosition - 15;
+  }
+
+  if (e.code === "KeyX") {
+    verticalPosition = verticalPosition + 15;
+  }
+  if (position < 0) {
+    position = 0;
+  }
+
+  if (verticalPosition < 0) {
+    verticalPosition = 0;
+  }
+
+  refresh();
 }
 
-function refresh(){
-    ball.style.top = yPosition + 'px';
-    ball.style.left = position + 'px';
+function refresh() {
+  ball.style.left = position + "px";
+  ball.style.top = verticalPosition + "px";
 }
