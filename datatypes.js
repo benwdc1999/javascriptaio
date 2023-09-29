11c5c5be045a8064eec74e245c1d57a55468896b
benjamin <benbhbe@yahoo.com>
2023-09-29 12:44:00 -0400

    Added files

---
+++ datatypes.js
@@ -0,0 +1,29 @@
+let phoneNumber;
+phoneNumber = "867-5309";
+
+console.log(phoneNumber);
+
+let favoriteColor = "grey";
+
+console.log(favoriteColor);
+
+let seven = 7;
+let eight = 8;
+let sum = seven + eight;
+
+console.log(sum);
+
+let firstName = "Benjamin";
+let lastName = "Dickinson";
+
+let fullName = firstName + " " + lastName;
+
+console.log(fullName);
+
+firstName = "Chuckles";
+
+console.log(firstName);
+
+console.log(fullName);
+
+let catName = "Mr. Furley";
